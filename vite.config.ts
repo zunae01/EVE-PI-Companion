7a625d8d9d968298a424019ca8b57acc95c87670
Montanaro Sandro <U240FL@voip.kpbs.ch>
2025-12-29 14:02:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ test
 export default defineConfig({
   plugins: [react()],
   resolve: {
